2d6a33481b124167c0934e666b0ae83e25c13b18
behrami <behram1993@gmail.com>
2018-06-19 22:43:35 -0400

    made link for comments on top_stories

--- app/controllers/top_stories_controller.rb
+++ app/controllers/top_stories_controller.rb
@@ -9,6 +9,7 @@ class TopStoriesController < ApplicationController
       single_response = HTTParty.get("https://hacker-news.firebaseio.com/v0/item/#{top_response[i]}.json")
 
       single_story_hash = {}
+      single_story_hash[:id] = single_response["id"]
       single_story_hash[:title] = single_response["title"]
       single_story_hash[:author] = single_response["by"]
       single_story_hash[:url] = single_response["url"]
